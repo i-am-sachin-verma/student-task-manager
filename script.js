3b1824737c2aa2f096060a4ff01e966d4dc8bcba
priyakalamkar555-art <priyakalamkar555@gmail.com>
2026-04-21 00:05:13 +0530

    Updated timestamp when editing a task

--- script.js
+++ script.js
@@ -3,45 +3,60 @@ function addTask() {
   const task = input.value.trim();
   const errorMsg = document.getElementById("errorMsg");
 
-  if (task.trim()=== "") {
+  if (task.trim() === "") {
     errorMsg.textContent = " Please enter a task.";
     return;
   };
   errorMsg.textContent = "";
+
   const li = document.createElement("li");
   const span = document.createElement("span");
   span.textContent = task;
+
   const now = new Date();
   const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const day = dayNames[now.getDay()];
-  const date =`${now.getDate()} ${now.toLocaleString("default", { month: "long" })} ${now.getFullYear()}`;
+  const date = `${now.getDate()} ${now.toLocaleString("default", { month: "long" })} ${now.getFullYear()}`;
   const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
   const timeElement = document.createElement("small");
   timeElement.textContent = ` (${day}, ${date} at ${time})`;
   timeElement.style.marginLeft = "10px";
   timeElement.style.color = "#888";
 
   const editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-    editButton.addEventListener("click", function () {
-      const newTask = prompt("Edit task:", span.textContent);
-      if (newTask !== null) {
-        span.textContent = newTask;
-      }
-    });
-    li.appendChild(span);
-    li.appendChild(timeElement);
-    li.appendChild(editButton);
+  editButton.textContent = "Edit";
+  editButton.addEventListener("click", function () {
+    const newTask = prompt("Edit task:", span.textContent);
+
+    if (newTask !== null && newTask.trim() !== "") {
+      span.textContent = newTask;
+
+      const now = new Date();
+      const day = dayNames[now.getDay()];
+      const date = `${now.getDate()} ${now.toLocaleString("default", { month: "long" })} ${now.getFullYear()}`;
+      const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+      timeElement.textContent = ` (${day}, ${date} at ${time})`;
+    }
+  });
+
+  li.appendChild(span);
+  li.appendChild(timeElement);
+  li.appendChild(editButton);
+
   const removeButton = document.createElement("button");
   removeButton.textContent = "Remove";
   removeButton.addEventListener("click", function () {
     li.remove();
   });
+
   li.appendChild(removeButton);
   document.getElementById("taskList").appendChild(li);
-  
+
   input.value = "";
 }
+
 function toggleTheme() {
   const body = document.body;
   const btn = document.getElementById("themeToggle");
